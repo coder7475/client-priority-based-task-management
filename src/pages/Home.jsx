923b69e22b0105b977e6756bac297b986240344f
coder7475 <robiulhossain7475@gmail.com>
2023-12-21 20:17:19 +0600

    feat: add feature section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Navbar from './../components/shared/Navbar';
 import Banner from './../components/ui/Banner';
 import Footer from './../components/shared/Footer';
+import Trusted from './../components/ui/Trusted';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Banner />
+      <Trusted />
       <Footer />
     </div>
   );
