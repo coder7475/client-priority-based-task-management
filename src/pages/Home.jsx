d7356ae5c5529a10f7074564351bbffd95557ec6
coder7475 <robiulhossain7475@gmail.com>
2023-12-21 18:37:55 +0600

    feat: add banner section of homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import Navbar from './../components/shared/Navbar';
+import Banner from './../components/ui/Banner';
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
-      Homepage
+      <Banner/>
     </div>
   );
 };
