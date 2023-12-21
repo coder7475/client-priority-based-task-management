0f8c7f03750e3bf43f9f3a54274bb5bcfce109a2
coder7475 <robiulhossain7475@gmail.com>
2023-12-21 13:28:19 +0600

    feat: put hover style for nav links

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -2,16 +2,16 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="max-w-4xl mx-auto sticky bg-slate-100">
-      <nav className="w-full py-2 shadow-lg">
-        <ul className="flex flex-col-reverse md:flex-row flex-wrap flex-1 justify-around items-center">
-          <NavLink to="/">
+    <div className="sticky bg-slate-100">
+      <nav className="w-full py-2 shadow-lg text-gray-600">
+        <ul className="max-w-4xl mx-auto flex flex-col-reverse md:flex-row flex-wrap flex-1 justify-around items-center">
+          <NavLink to="/" className="hover:bg-slate-500 hover:text-white p-2 rounded-sm">
             Home
           </NavLink>
-          <NavLink to="/register">
+          <NavLink to="/register" className="hover:bg-slate-500 hover:text-white p-2 rounded-sm">
             Registration
           </NavLink>
-          <NavLink to="/login">
+          <NavLink to="/login" className="hover:bg-slate-500 hover:text-white p-2 rounded-sm">
             Login
           </NavLink>
         </ul>
