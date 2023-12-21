f56e061e83596acea052a31d9681739dd66c24c9
coder7475 <robiulhossain7475@gmail.com>
2023-12-21 18:50:24 +0600

    feat: add footer with clickable social media link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from './../components/shared/Navbar';
 import Banner from './../components/ui/Banner';
+import Footer from './../components/shared/Footer';
 
 const Home = () => {
   return (
     <div>
-      <Navbar/>
-      <Banner/>
+      <Navbar />
+      <Banner />
+      <Footer />
     </div>
   );
 };
