01a60864bd86aad235de56ed046b4b409019d7e7
coder7475 <robiulhossain7475@gmail.com>
2023-12-21 13:10:17 +0600

    feat: create the root layout

--- src/routers/public.jsx
+++ src/routers/public.jsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
-import App from './../App';
 import Error from './../pages/Error';
+import Home from './../pages/Home';
+import RootLayout from './../layouts/RootLayout';
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
-    errorElement: <Error/>
+    element: <RootLayout/>,
+    errorElement: <Error/>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      }
+    ]
   }
 ])
