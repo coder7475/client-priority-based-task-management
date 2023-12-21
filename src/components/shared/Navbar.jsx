099623c3e4c99fc3d2184614eecb232352894c4d
coder7475 <robiulhossain7475@gmail.com>
2023-12-21 13:37:10 +0600

    feat: add active navbar styling

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -4,16 +4,37 @@ const Navbar = () => {
   return (
     <div className="sticky bg-slate-100">
       <nav className="w-full py-2 shadow-lg text-gray-600">
-        <ul className="max-w-4xl mx-auto flex flex-col-reverse md:flex-row flex-wrap flex-1 justify-around items-center">
-          <NavLink to="/" className="hover:bg-slate-500 hover:text-white p-2 rounded-sm">
-            Home
-          </NavLink>
-          <NavLink to="/register" className="hover:bg-slate-500 hover:text-white p-2 rounded-sm">
-            Registration
-          </NavLink>
-          <NavLink to="/login" className="hover:bg-slate-500 hover:text-white p-2 rounded-sm">
-            Login
-          </NavLink>
+        <ul className="max-w-2xl mx-auto flex flex-col-reverse md:flex-row flex-wrap flex-1 justify-around items-center">
+          <li className="hover:bg-slate-500 hover:text-white p-2 rounded-sm">
+            <NavLink
+              to="/"
+              className={({ isActive, isPending }) =>
+                isActive ? "font-bold" : isPending ? "font-lg" : ""
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="hover:bg-slate-500 hover:text-white p-2 rounded-sm">
+            <NavLink
+              to="/register"
+              className={({ isActive, isPending }) =>
+                isActive ? "font-bold" : isPending ? "font-lg" : ""
+              }
+            >
+              Registration
+            </NavLink>
+          </li>
+          <li className="hover:bg-slate-500 hover:text-white p-2 rounded-sm">
+            <NavLink
+              to="/login"
+              className={({ isActive, isPending }) =>
+                isActive ? "font-bold" : isPending ? "font-lg" : ""
+              }
+            >
+              Login
+            </NavLink>
+          </li>
         </ul>
       </nav>
     </div>
