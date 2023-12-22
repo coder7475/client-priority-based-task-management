46b5817253e41741c09ec50b7f35c4a9e7bd4669
coder7475 <robiulhossain7475@gmail.com>
2023-12-22 11:16:38 +0600

    feat: style the to do lists

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -162,7 +162,7 @@ const DashboardLayout = () => {
           onClick={() => setIsSideNavOpen(false)}
         ></div>
       </div>
-      <div className="ml-24 mr-5 mt-5">
+      <div className="ml-24 mr-5 mt-5 w-3/4">
         <Outlet />
       </div>
     </div>
